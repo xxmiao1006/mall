bb60465fc0f487987b009392a699eb3c98b6be69
xxmiao1006 <1151856023@qq.com>
2020-09-10 11:32:55 +0800

    zuul filter

---
+++ zuul-proxy/src/main/java/com/macro/cloud/filter/PreLogFilter.java
@@ -0,0 +1,57 @@
+package com.macro.cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PreLogFilter extends ZuulFilter {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 过滤器类型，有pre、routing、post、error四种。
+     */
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    /**
+     * 过滤器执行顺序，数值越小优先级越高。
+     */
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    /**
+     * 是否进行过滤，返回true会执行过滤。
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 自定义的过滤器逻辑，当shouldFilter()返回true时会执行。
+     */
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String remoteHost = request.getRemoteHost();
+        String requestMethod = request.getMethod();
+        String requestURI = request.getRequestURI();
+        String debug = request.getParameter("debug");
+        LOGGER.info("Remote host:{},method:{},uri:{},debug:{}", remoteHost, requestMethod, requestURI,debug);
+        return null;
+    }
+}
